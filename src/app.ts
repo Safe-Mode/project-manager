c4c7402ddd20ddd7526524d5e9ab2ecebf1aa2d8
Sergey Dmitriev <dmitriev@reksoft.ru>
2021-09-15 11:24:29 +0300

    Select, configure and attach elements. Implement AutoBind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,49 @@
-// Code goes here!
+const AutoBind = (
+    _target: any,
+    _methodName: string,
+    { value, configurable, enumerable }: PropertyDescriptor
+): PropertyDescriptor => ({
+  configurable,
+  enumerable,
+  get() {
+    return value.bind(this);
+  }
+});
+
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
+  constructor() {
+    this.templateEl = document.querySelector('#project-input')!;
+    this.hostEl = document.querySelector('#app')!;
+    this.element = document.importNode(this.templateEl.content, true).firstElementChild as HTMLFormElement;
+    this.titleInputEl = this.element.querySelector('#title')!;
+    this.descriptionInputEl = this.element.querySelector('#description')!;
+    this.peopleInputEl = this.element.querySelector('#people')!;
+
+    this.configure();
+    this.attach();
+  }
+
+  @AutoBind
+  private submitHandler(evt: Event) {
+    evt.preventDefault();
+    console.log(this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.element.id = 'user-input';
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+new ProjectInput();
